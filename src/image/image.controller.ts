902d73d33a4092cbebb0d52a7b83445a32df5db3
mklebe <matthias.klebe@gmail.com>
2020-03-11 22:28:36 +0100

    Saves Image on the server

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express'
-import { ApiOperation, ApiTags, ApiConsumes, ApiProperty, ApiBody } from '@nestjs/swagger'
+import { ApiOperation, ApiTags, ApiConsumes, ApiProperty, ApiBody, ApiResponse } from '@nestjs/swagger'
+import { diskStorage } from  'multer'
+import { extname } from  'path'
 
 class FileUploadDto {
     @ApiProperty({ type: 'string', format: 'binary' })
@@ -10,9 +12,18 @@ class FileUploadDto {
 @ApiTags('image')
 @Controller('image')
 export class ImageController {
-    @Post('upload')
+    @Post('upload/recipe')
     @UseInterceptors(
-        FileInterceptor('file'),
+        FileInterceptor('file', {
+            storage: diskStorage({
+                destination: './images/recipe',
+                filename: (req, file, cb) => {
+                    console.log( req )
+                    const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
+                    return cb(null, `${randomName}${extname(file.originalname)}`)
+                }
+            })
+        }),
     )
     @ApiOperation({operationId: 'uploadImage'})
     @ApiBody({
@@ -21,7 +32,6 @@ export class ImageController {
     })
     @ApiConsumes('multipart/form-data')
     async uploadFile( @UploadedFile() file ) {
-        console.log( 'File upload' )
-        console.log( file )
+        return file.path
     }
 }
