2b16e55510a277c399f10ed608f4ae65603736ca
mklebe <matthias.klebe@gmail.com>
2020-04-02 22:21:24 +0200

    Cleans images from filesystem after uploading to cloud

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -3,6 +3,7 @@ import { FileInterceptor } from '@nestjs/platform-express'
 import { ApiOperation, ApiTags, ApiConsumes, ApiProperty, ApiBody, ApiResponse } from '@nestjs/swagger'
 import { diskStorage } from  'multer'
 import { extname } from  'path'
+const fs = require('fs')
 const cloudinary = require('cloudinary')
 
 class NestImage {
@@ -24,7 +25,6 @@ export class ImageController {
             storage: diskStorage({
                 destination: './static/images/recipe',
                 filename: (req, file, cb) => {
-                    console.log( req )
                     const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
                     return cb(null, `${randomName}${extname(file.originalname)}`)
                 }
@@ -42,10 +42,14 @@ export class ImageController {
     })
     @ApiConsumes('multipart/form-data')
     async uploadFile( @UploadedFile() file ) {
-        console.log(`### Upload ${file.path} ###`)
-        cloudinary.uploader.upload( file.path, (result) => {
-            console.log( result )
+        let image 
+        const filePath = file.path
+        await cloudinary.uploader.upload( filePath, (result) => {
+            image = result
         })
-        return {src: file.path}
+        fs.unlink(filePath, () => {
+            console.log( `### removed ${filePath} ###` )
+        })
+        return image
     }
 }
